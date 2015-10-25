8869daf13935d2b8dfac144fafd2254df7be9fea
Jarek Kaspar <kaspar.jarek@gmail.com>
2015-10-25 21:27:25 +0100

    Implement LoanItemDao

--- library-persistence/src/main/java/cz/muni/fi/pa165/projects/library/persistence/dao/LoanItemDao.java
+++ library-persistence/src/main/java/cz/muni/fi/pa165/projects/library/persistence/dao/LoanItemDao.java
@@ -1,7 +1,45 @@
 package cz.muni.fi.pa165.projects.library.persistence.dao;
 
+import cz.muni.fi.pa165.projects.library.persistence.entity.Loan;
+import cz.muni.fi.pa165.projects.library.persistence.entity.LoanItem;
+
+import java.util.List;
+
 /**
  * @author jkaspar
  */
 public interface LoanItemDao {
+
+    /**
+     * Persis new LoanItem into persistence context
+     * @param loanItem
+     */
+    public void create(LoanItem loanItem);
+
+    /**
+     * Remove LoanItem from persistence context
+     * @param loanItem
+     */
+    public void delete(LoanItem loanItem);
+
+    /**
+     * Find LoanItem by his id
+     * @param id
+     * @return LoanItem with given id
+     */
+    public LoanItem findById(Long id);
+
+    /**
+     * Find all LoanItems which belongs to given loan
+     * @param loan
+     * @return list of LoanItems
+     */
+    public List<LoanItem> findByLoan(Loan loan);
+
+
+    /**
+     * Find all LoanItems from the database
+     * @return list of all LoanItems
+     */
+    public List<LoanItem> findAll();
 }
--- library-persistence/src/main/java/cz/muni/fi/pa165/projects/library/persistence/dao/LoanItemDaoImpl.java
+++ library-persistence/src/main/java/cz/muni/fi/pa165/projects/library/persistence/dao/LoanItemDaoImpl.java
@@ -1,7 +1,48 @@
 package cz.muni.fi.pa165.projects.library.persistence.dao;
 
+import cz.muni.fi.pa165.projects.library.persistence.entity.Loan;
+import cz.muni.fi.pa165.projects.library.persistence.entity.LoanItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * @author jkaspar
  */
-public class LoanItemDaoImpl {
+public class LoanItemDaoImpl implements LoanItemDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(LoanItem loanItem) {
+        Objects.requireNonNull(loanItem.getBook(), "Book is not specified");
+        Objects.requireNonNull(loanItem.getLoan(), "Loan is not specified");
+        em.persist(loanItem);
+    }
+
+    @Override
+    public void delete(LoanItem loanItem) {
+        em.remove(loanItem);
+    }
+
+    @Override
+    public LoanItem findById(Long id) {
+        return em.find(LoanItem.class, id);
+    }
+
+    @Override
+    public List<LoanItem> findByLoan(Loan loan) {
+        Objects.requireNonNull(loan, "Loan was null");
+        return em.createQuery("SELECT l FROM LoanItem WHERE l.loan = :loanid", LoanItem.class)
+                .setParameter("loanid", loan)
+                .getResultList();
+    }
+
+    @Override
+    public List<LoanItem> findAll() {
+        return em.createQuery("SELECT l FROM LoanItem l", LoanItem.class).getResultList();
+    }
 }
