f1cf67ed99551ded9f0aa2a8cb2c779b18cef270
Jarek Kaspar <kaspar.jarek@gmail.com>
2015-11-21 23:45:27 +0100

    Add LoanItem facade

---
+++ library-service/src/main/java/cz/muni/fi/pa165/projects/library/service/facade/LoanItemFacadeImpl.java
@@ -0,0 +1,59 @@
+package cz.muni.fi.pa165.projects.library.service.facade;
+
+import cz.muni.fi.pa165.projects.library.dto.LoanDTO;
+import cz.muni.fi.pa165.projects.library.dto.LoanItemCreateDTO;
+import cz.muni.fi.pa165.projects.library.dto.LoanItemDTO;
+import cz.muni.fi.pa165.projects.library.facade.LoanItemFacade;
+import cz.muni.fi.pa165.projects.library.persistence.entity.Loan;
+import cz.muni.fi.pa165.projects.library.persistence.entity.LoanItem;
+import cz.muni.fi.pa165.projects.library.service.BeanMappingService;
+import cz.muni.fi.pa165.projects.library.service.LoanItemService;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * @author Jaroslav Kaspar
+ */
+public class LoanItemFacadeImpl implements LoanItemFacade {
+
+    @Inject
+    private LoanItemService loanItemService;
+
+    @Inject
+    private BeanMappingService beanMappingService;
+
+    @Override
+    public Long createLoanItem(LoanItemCreateDTO loanItemCreateDTO) {
+        LoanItem loanItem = beanMappingService.mapTo(loanItemCreateDTO, LoanItem.class);
+        loanItemService.create(loanItem);
+        return loanItem.getId();
+    }
+
+    @Override
+    public void deleteLoanItem(LoanItemDTO loanItemDTO) {
+        loanItemService.delete(beanMappingService.mapTo(loanItemDTO, LoanItem.class));
+    }
+
+    @Override
+    public void updateLoanItem(LoanItemDTO loanItemDTO) {
+        loanItemService.update(beanMappingService.mapTo(loanItemDTO, LoanItem.class));
+    }
+
+    @Override
+    public LoanItemDTO findLoanItemById(Long loanItemId) {
+        LoanItem loanItem = loanItemService.findById(loanItemId);
+        return (loanItem == null) ? null : beanMappingService.mapTo(loanItem, LoanItemDTO.class);
+    }
+
+    @Override
+    public List<LoanItemDTO> findByLoan(LoanDTO loan) {
+        List<LoanItem> loanItems = loanItemService.findByLoan(beanMappingService.mapTo(loan, Loan.class));
+        return (loanItems == null) ? null : beanMappingService.mapTo(loanItems, LoanItemDTO.class);
+    }
+
+    @Override
+    public List<LoanItemDTO> findAll() {
+        return beanMappingService.mapTo(loanItemService.findAll(), LoanItemDTO.class);
+    }
+}
