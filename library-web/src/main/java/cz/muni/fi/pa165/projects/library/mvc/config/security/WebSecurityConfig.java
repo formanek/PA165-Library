ce430e7961c273b52768f3452519da212faa6959
Jarek Kaspar <kaspar.jarek@gmail.com>
2015-12-22 00:35:51 +0100

    web MVC hash passwords using bcrypt

--- library-web/src/main/java/cz/muni/fi/pa165/projects/library/mvc/config/security/WebSecurityConfig.java
+++ library-web/src/main/java/cz/muni/fi/pa165/projects/library/mvc/config/security/WebSecurityConfig.java
@@ -1,12 +1,15 @@
 package cz.muni.fi.pa165.projects.library.mvc.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static cz.muni.fi.pa165.projects.library.mvc.config.security.UserRoles.ADMIN;
 import static cz.muni.fi.pa165.projects.library.mvc.config.security.UserRoles.LIBRARIAN;
@@ -30,10 +33,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("both").password("123").authorities(ADMIN, LIBRARIAN).and()
-            .withUser("admin").password("123").authorities(ADMIN).and()
-            .withUser("librarian").password("123").authorities(LIBRARIAN);
+            .passwordEncoder(passwordEncoder)
+            .withUser("admin").password(passwordEncoder.encode("123")).authorities(ADMIN).and()
+            .withUser("librarian").password(passwordEncoder.encode("123")).authorities(LIBRARIAN).and()
+            .withUser("both").password(passwordEncoder.encode("123")).authorities(ADMIN, LIBRARIAN);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
